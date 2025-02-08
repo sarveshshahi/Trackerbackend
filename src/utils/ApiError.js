7250ffe9d29c085e4caad711422c4d305ded68ea
SARVESH SHAHI <118737457+sarveshshahi@users.noreply.github.com>
2025-02-08 19:57:23 +0530

    Refactor database connection handling, enhance ApiError class, and add ApiResponse utility

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
         message= "Something went wrong",
@@ -6,10 +6,19 @@ class ApiError extends Error{
         stack = ""
     ){
         super(message)
-        this.statusCode=statusCode
-        this.data=null
-        this.message=message
-        this.success=false;
-        this.errors=errors
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
     }
 }
+
+export {ApiError}
